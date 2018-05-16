f4069da9d1e7069bf55b4a46fff3a49171bf26ac
Patricio Aumedes <patricioaumedes@gmail.com>
2018-05-15 21:34:00 -0300

    Creacion de clases

---
+++ classes/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+    abstract class auth
+    {
+        // PASA EL PARAMETRO RECORDAR. SINO SE PASA EL DEFAULT ES IGUAL A CERO
+        public static function login($remember = 0)
+        {
+
+        }
+
+        public static function logout()
+        {
+
+        }
+
+        public static function isLogged()
+        {
+            return boolean;
+        }
+
+
+
+
+    }
+
+
+?>
